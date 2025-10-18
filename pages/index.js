ed12d36bace800723b810169f43ac1a95d774d93
guidogabriel <guido.gabriel1987@gmail.com>
2025-10-18 02:37:46 +0000

    Adicionando scripts de linting com Prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Alterando o texto para um texto oficial. Bora querido!</h1>
+  return <h1>Alterando o texto para um texto oficial. Bora querido!</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
