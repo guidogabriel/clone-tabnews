856ac0ce586ea110a7ce9d4ed3c1236ba2db89a4
guidogabriel <guido.gabriel1987@gmail.com>
2025-10-25 17:11:10 +0000

    adiciona scripts para gerenciar serviços com Docker Compose

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 503 AS result;");
+  const result = await database.query("SELECT 3+7 AS resultadoeh;");
 
   console.log(result.rows);
   response.status(200).json({ status: "ok" });
