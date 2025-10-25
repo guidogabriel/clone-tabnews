84f244557760c57a71ac82c5c6196c5e75228fd1
guidogabriel <guido.gabriel1987@gmail.com>
2025-10-25 16:02:59 +0000

    Adiciona configurações de banco de dados via variáveis de ambiente

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 503 AS result;");
+
+  console.log(result.rows);
   response.status(200).json({ status: "ok" });
 }
 
